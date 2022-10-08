1d9410ce8d9732a80280e9eab42c72f2427ba982
joshyeom <jhyeom95@naver.com>
2022-10-08 19:39:46 +0900

    10/07

---
+++ 10-06/app.js
@@ -0,0 +1,10 @@
+let left = document.querySelector(".fa-angle-left");
+let right = document.querySelector(".fa-angle-right");
+let box = document.querySelector(".box");
+
+function moveLeft() {}
+
+function moveRight() {}
+
+addEventListener("click", moveLeft);
+addEventListener("click", moveRight);
---
+++ Attendence Book/app.js
@@ -0,0 +1,49 @@
+let search = document.querySelector("input");
+let body = document.querySelector("body");
+let li = document.querySelectorAll("li");
+let buttonO = document.querySelectorAll(".button-o");
+let buttonX = document.querySelectorAll(".button-x");
+
+function putName(event) {
+  event.preventDefault();
+  for (let i = 0; i < li.length; i++) {
+    if (search.value == li[i].textContent) {
+      li[i].style.backgroundColor = "skyBlue";
+    }
+  }
+}
+
+let attendanceList1 = document.querySelector("#attendance-list1");
+let attendanceList2 = document.querySelector("#attendance-list2");
+let attendanceList3 = document.querySelector("#attendance-list3");
+let absentList1 = document.querySelector("#absent-list1");
+let absentList2 = document.querySelector("#absent-list2");
+
+function attendanceO(i) {
+  attendanceList1.append(buttonO[i].parentElement);
+  if (attendanceList1.childNodes.length > 10) {
+    attendanceList2.append(buttonO[i].parentElement);
+    if (attendanceList2.childNodes.length > 10) {
+      attendanceList3.append(buttonO[i].parentElement);
+    }
+  }
+}
+
+function attendanceX(i) {
+  absentList1.append(buttonX[i].parentElement);
+}
+
+body.addEventListener("submit", putName);
+
+for (let i = 0; i < buttonO.length; i++) {
+  buttonO[i].addEventListener("click", function () {
+    attendanceO(i);
+  });
+}
+for (let i = 0; i < buttonX.length; i++) {
+  buttonX[i].addEventListener("click", function () {
+    attendanceX(i);
+  });
+}
+
+// buttonX.addEventListener("click", attendanceX);
