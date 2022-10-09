acb86be2a5e64ad4cbcd7e5799b40640671fdb82
joshyeom <jhyeom95@naver.com>
2022-10-09 16:24:32 +0900

    OX 버튼 수정, 출석 / 결석 버튼 삭제 후 focus 되게, 결석버튼 누르고 상세 나누기

--- Attendence Book/app.js
+++ Attendence Book/app.js
@@ -1,16 +1,8 @@
 let search = document.querySelector("input");
 let body = document.querySelector("body");
-let li = document.querySelectorAll("li");
-let buttonO = document.querySelectorAll(".button-o");
-
-function putName(event) {
-  event.preventDefault();
-  for (let i = 0; i < li.length; i++) {
-    if (search.value == li[i].textContent) {
-      li[i].style.backgroundColor = "skyBlue";
-    }
-  }
-}
+let ol = document.querySelectorAll("ol");
+let buttonAttendance = document.querySelectorAll(".button-attendance");
+let buttonAbsent = document.querySelectorAll(".button-absent");
 
 let attendanceList1 = document.querySelector("#attendance-list1");
 let attendanceList2 = document.querySelector("#attendance-list2");
@@ -18,32 +10,40 @@ let attendanceList3 = document.querySelector("#attendance-list3");
 let absentList1 = document.querySelector("#absent-list1");
 let absentList2 = document.querySelector("#absent-list2");
 
-function attendanceO(i) {
-  attendanceList1.append(buttonO[i].parentElement);
-  if (attendanceList1.childNodes.length > 10) {
-    attendanceList2.append(buttonO[i].parentElement);
-    if (attendanceList2.childNodes.length > 10) {
-      attendanceList3.append(buttonO[i].parentElement);
+function attendance(i) {
+  attendanceList1.append(buttonAttendance[i].parentElement);
+  if (attendanceList1.childNodes.length > 6) {
+    attendanceList2.append(buttonAttendance[i].parentElement);
+    if (attendanceList2.childNodes.length > 6) {
+      attendanceList3.append(buttonAttendance[i].parentElement);
     }
   }
 }
 
-function attendanceX(i) {
-  absentList1.append(buttonX[i].parentElement);
-}
-
-body.addEventListener("submit", putName);
-
-for (let i = 0; i < buttonO.length; i++) {
-  buttonO[i].addEventListener("click", function () {
-    attendanceO(i);
+for (let i = 0; i < buttonAttendance.length; i++) {
+  buttonAttendance[i].addEventListener("click", function () {
+    attendance(i);
   });
 }
 
-for (let i = 0; i < buttonX.length; i++) {
-  buttonX[i].addEventListener("click", function () {
-    attendanceX(i);
+function absent(i) {
+  absentList1.append(buttonAbsent[i].parentElement);
+  if (absentList1.childNodes.length > 10) {
+    absentList2.append(buttonAbsent[i].parentElement);
+    if (absentList2.childNodes.length > 10) {
+      absentList3.append(buttonAbsent[i].parentElement);
+    }
+  }
+}
+
+function absent(i) {
+  absentList1.append(buttonAbsent[i].parentElement);
+}
+
+for (let i = 0; i < buttonAbsent.length; i++) {
+  buttonAbsent[i].addEventListener("click", function () {
+    absent(i);
   });
 }
 
-// buttonX.addEventListener("click", attendanceX);
+// buttonAbsent.addEventListener("click", absent);
